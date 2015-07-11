8db815ea8f9466ade47145bcac5ac750ce07a64e
Ievgen <ievgen.bezuglyi@gmail.com>
2015-07-11 11:30:09 +0200

    state

---
+++ curls/state1.js
@@ -0,0 +1,61 @@
+action=bet_request&game_state={
+    "tournament_id": "5587222ded8a3e0003000002",
+    "game_id": "55a0d3a652bc730003000041",
+    "round": 0,
+    "players": [{
+      "name": "RoyalFlushFactory",
+      "stack": 1000,
+      "status": "active",
+      "bet": 0,
+      "hole_cards": [],
+      "version": "Mad player",
+      "id": 0
+    }, {
+      "name": "Admiral Ackbar",
+      "stack": 1000,
+      "status": "active",
+      "bet": 0,
+      "hole_cards": [],
+      "version": "Admiral Ackbar lives!",
+      "id": 1
+    }, {
+      "name": "Injectionjs",
+      "stack": 1000,
+      "status": "active",
+      "bet": 0,
+      "hole_cards": [],
+      "version": "Chuck Norris player",
+      "id": 2
+    }, {
+      "name": "FunnyPokerBot",
+      "stack": 1000,
+      "status": "active",
+      "bet": 0,
+      "hole_cards": [],
+      "version": "Default C# folding player",
+      "id": 3
+    }, {
+      "name": "LeanSharp",
+      "stack": 1000,
+      "status": "active",
+      "bet": 0,
+      "hole_cards": [],
+      "version": "Default C# folding player",
+      "id": 4
+    }, {
+      "name": "MJ",
+      "stack": 1000,
+      "status": "active",
+      "bet": 0,
+      "hole_cards": [],
+      "version": "Default C++ folding player",
+      "id": 5
+    }],
+    "minimum_raise":100,
+    "small_blind": 10,
+    "orbits": 0,
+    "dealer": 2,
+    "community_cards": [],
+    "current_buy_in": 0,
+    "pot": 0
+  }
--- player.js
+++ player.js
@@ -1,33 +1,34 @@
 module.exports = {
-    VERSION: "Chuck Norris player",
-    bet_request: function(game_state) {
-        var bet = 1+ this.ijs_getMinimumRaise(game_state);
-        console.log(bet);
-        return bet;
+  VERSION: "Chuck Norris player",
+  bet_request: function(game_state) {
+    console.log(game_state);
+    var bet = 1 + this.ijs_getMinimumRaise(game_state);
+    console.log(bet);
+    return bet;
+  },
 
+  showdown: function(game_state) {
 
-    },
+  },
 
-    showdown: function(game_state) {
+  ijs_getMinimumRaise: function(game_state) {
+    console.log("state:", game_state);
+    console.log("min:", game_state.minimum_raise);
+    return game_state.minimum_raise;
+  },
 
-    },
+  ijs_getCurrentBuyIn: function(game_state) {
+    return game_state.current_buy_in;
+  },
 
-    ijs_getMinimumRaise:function(game_state){
-        return game_state.minimum_raise;
-    },
+  ijs_findMaxBetOfActivePlayer: function(game_state) {
+    var maxBet = 0;
+    game_state.players.forEach(function(player, index) {
+      if (player.status === 'active') {
+        maxBet += player.bet;
+      }
+    });
 
-    ijs_getCurrentBuyIn: function(game_state) {
-        return game_state.current_buy_in;
-    },
-
-    ijs_findMaxBetOfActivePlayer: function(game_state) {
-        var maxBet = 0;
-        game_state.players.forEach(function(player, index) {
-            if (player.status === 'active') {
-                maxBet += player.bet;
-            }
-        });
-
-        return maxBet;
-    }
+    return maxBet;
+  }
 };
