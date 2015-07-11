52896fd81ab5127accdf30746650551c20783b14
jstadnicki <jaroslaw.stadnicki@gmail.com>
2015-07-11 10:08:15 +0200

    max bet

--- player.js
+++ player.js
@@ -1,13 +1,28 @@
-
 module.exports = {
 
-  VERSION: "Default JavaScript folding player",
+    VERSION: "Default JavaScript folding player",
 
-  bet_request: function(game_state) {
-    return 0;
-  },
+    bet_request: function(game_state) {
+        return getCurrentBuyIn(game_state) - findMaxBetOfActivePlayer(game_state) + 1;
+    },
 
-  showdown: function(game_state) {
+    showdown: function(game_state) {
 
-  }
+    },
+
+    getCurrentBuyIn: function(game_state) {
+        return game_state.current_buy_in;
+    },
+
+
+    findMaxBetOfActivePlayer: function(game_state) {
+        var maxBet = 0;
+        game_state.players.forEach(function(player, index) {
+            if (player.status === 'active' && player.bet > maxBet) {
+                maxBet = player.bet;
+            }
+        });
+
+        return maxBet;
+    }
 };
