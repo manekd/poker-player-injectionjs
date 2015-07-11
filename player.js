e1f8f0c42a3cf9aa8799baca3053122b37943da5
jstadnicki <jaroslaw.stadnicki@gmail.com>
2015-07-11 10:26:09 +0200

    pre

--- player.js
+++ player.js
@@ -1,22 +1,22 @@
 module.exports = {
     VERSION: "Chuck Norris player",
     bet_request: function(game_state) {
-        return getCurrentBuyIn(game_state) - findMaxBetOfActivePlayer(game_state) + 1+getMinimumRaise(game_state);
+        return ijs_getCurrentBuyIn(game_state) - ijs_findMaxBetOfActivePlayer(game_state) + 1+ijs_getMinimumRaise(game_state);
     },
 
     showdown: function(game_state) {
 
     },
 
-    getMinimumRaise:function(game_state){
+    ijs_getMinimumRaise:function(game_state){
         return game_state.minimum_raise;
     },
 
-    getCurrentBuyIn: function(game_state) {
+    ijs_getCurrentBuyIn: function(game_state) {
         return game_state.current_buy_in;
     },
 
-    findMaxBetOfActivePlayer: function(game_state) {
+    ijs_findMaxBetOfActivePlayer: function(game_state) {
         var maxBet = 0;
         game_state.players.forEach(function(player, index) {
             if (player.status === 'active' && player.bet > maxBet) {
